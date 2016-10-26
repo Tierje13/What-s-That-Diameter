028908bb6a073d621da3e85ca4938e9555221f3f
Tierje13 <tierje11@gmail.com>
2016-10-26 12:45:07 -0600

    Final

---
+++ Circle.java
@@ -0,0 +1,30 @@
+import java.lang.Math;
+
+public class Circle {
+private double radius;
+public Circle(double r){
+	radius=r;
+}
+public double area(){
+	double a=Math.PI*radius*radius;
+	return a;
+}
+public double circumference(){
+	double c= 2*Math.PI*radius;
+	return c;
+}
+public double diameter(){
+	double d= radius*2;
+	return d;
+}
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
---
+++ Tester.java
@@ -0,0 +1,10 @@
+
+public class Tester {
+
+	public static void main(String[] args) {
+		Circle cir1=new Circle(35.5);
+		System.out.println(cir1.diameter());
+
+	}
+
+}
